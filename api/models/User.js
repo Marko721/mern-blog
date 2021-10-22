9f8074cae9b2a431c4c77d4944e5c5bccab4f137
unknown <popmark1002@gmail.com>
2021-10-23 00:46:22 +0200

    Created DB Models

---
+++ api/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+    // we can make createdAt and updatedAt but we use timestamps instead
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
