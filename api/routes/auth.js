fc7612103b9ba6a0ae0c0602128ad981556f3482
unknown <popmark1002@gmail.com>
2021-10-26 00:09:38 +0200

    Adding user over Postman should work

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,1 +1,24 @@
 const router = require("express").Router();
+
+const { response } = require("express");
+const User = require("../models/User");
+
+// REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    const user = await newUser.save();
+    response.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// LOGIN
+
+module.exports = router;
