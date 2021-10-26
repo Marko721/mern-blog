aa791378249aeae219212f1b699b783273bedca4
Marko721 <popmark1002@gmail.com>
2021-10-26 13:08:17 +0200

    Successfully getting server status 200 when added a new user to MongoDB

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-
 const authRoute = require("./routes/auth");
 
 dotenv.config();
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -13,7 +13,7 @@ router.post("/register", async (req, res) => {
     });
 
     const user = await newUser.save();
-    response.status(200).json(user);
+    res.status(200).json(user);
   } catch (err) {
     res.status(500).json(err);
   }
